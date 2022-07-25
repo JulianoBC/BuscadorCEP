a323d4cd8c560098e21bd4d1bebe7bdb33a86453
JulianoBC <88047451+JulianoBC@users.noreply.github.com>
2022-07-25 14:00:18 -0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,65 @@
+import { useState} from 'react'
+import { FiSearch } from 'react-icons/fi';
+import './styles.css';
+//viacep.com.br/ws/${input}/jasons/
+import api from './services/api.js';
+
+function App() {
+
+  const [input, setInput] = useState('');
+  const [cep, setCep] = useState({});
+
+  async function handleSearch(){
+    if(input === '') {
+      alert('Preencha algum CEP !')
+      return; 
+    }
+
+    try{
+    const responsee = await api.get(`${input}/json/`);
+      setCep(responsee.data)
+      setInput("");
+
+    }catch{
+      alert("ops error ao buscar");
+      setInput("")
+    }
+  }
+
+  return (
+    <div className="container">
+      <h1 className="title">Buscador CEP</h1>
+
+      <div className="containerInput">
+        <input
+        type="text"
+        placeholder="Digite seu cep..."
+        value={input}
+        onChange={(event) => setInput(event.target.value) } 
+        />
+
+        <button className="buttonSearch" onClick={handleSearch}>
+          <FiSearch size={25} color="FFF"/>
+        </button>
+      </div>
+
+
+
+      {Object.keys(cep).length > 0 && (
+        
+        <main className="main">
+        <h2>CEP: {cep.cep}</h2>
+
+        <span>Rua: {cep.logradouro}</span>
+        <span>Complemento: {cep.complemento}</span>
+        <span>Bairro: {cep.bairro}</span>
+        <span>Cidade: {cep. localidade} - {cep.uf}</span>
+
+      </main>  
+      )}
+      
+    </div>
+  );
+}
+
+export default App;
